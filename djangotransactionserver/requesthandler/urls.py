417a42d7fe95b1a9d7dcfb2968bef5e21e591f1e
Curtis Smith <curtis.smith8@gmail.com>
2016-01-25 20:28:40 -0800

    basic web server is working, I now realise I have named it the transaction server.. will change that

--- djangotransactionserver/requesthandler/urls.py
+++ djangotransactionserver/requesthandler/urls.py
@@ -1,5 +1,6 @@
+from django.conf.urls import url
 
-from requesthandler import views
+from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
--- djangotransactionserver/requesthandler/views.py
+++ djangotransactionserver/requesthandler/views.py
@@ -1,5 +1,19 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+
 from django.http import HttpResponse
 
-
+@csrf_exempt
 def index(request):
-    return render(request, 'requesthandler/templates/index.html', {})
+    print 'sup'
+    if request.method == 'GET':
+        return render(request, 'requesthandler/index.html', {})
+    elif request.method == 'POST':
+        response = HttpResponse()
+        response.status_code = 200
+        print request.body
+        return response
+    else:
+        print "invalid request received"
+        return render(request, 'requesthandler/index.html', {})
+
