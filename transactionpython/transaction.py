19be28c6f2c22227a65ab2b6c253fa963afde6ab
Curtis Smith <curtis.smith8@gmail.com>
2016-02-02 15:07:28 -0800

    parse a dict and check request type

--- transactionpython/transaction.py
+++ transactionpython/transaction.py
@@ -3,13 +3,18 @@ import os
 import socket
 import string
 import sys
+import ast
 
 def process_request(data):
 	print "Gonna do some shit with this:"
 	print data
+	data_dict = ast.literal_eval(data)
+	for key, value in data_dict.iteritems():
+		print key + " is " + value + "\n"
 
+	if data_dict["request_type"] == "buy":
+		print "holy moly its a buy"
 	return
-
 
 host = ''
 port = 44421
