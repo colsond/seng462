c24e7124eeb0eb80a77f9a6c18a0b074ab22bc2b
Colson Driemel <colsond@uvic.ca>
2016-02-23 17:57:55 -0800

    add unique thread ports
    Workload generator now adds the thread id to the port number to get a range of ports for use.
    tested with 4 threads.

--- workloadgenerator/workloadGenerator.py
+++ workloadgenerator/workloadGenerator.py
@@ -3,6 +3,7 @@ import socket
 import sys
 import string
 import Queue
+import time
 from threading import Thread, current_thread
 
 
@@ -10,7 +11,8 @@ web_server_address = 'b132.seng.uvic.ca'
 # web_server_address = 'localhost'
 # Port list, in case things are run on same machine
 # 44421	Audit
-# 44422 Transaction
+# 44422-44425 Transaction ports, number below gets added to by the various thread ids (0-3)
+
 web_server_port = 44422
 
 ADD = "ADD"
@@ -33,7 +35,7 @@ NUM_WORKER_THREADS = 4
 
 q = Queue.Queue()
 
-def make_request(transaction_id, request_type, user=None, stock_id=None, amount=None, filename=None):
+def make_request(pid, transaction_id, request_type, user=None, stock_id=None, amount=None, filename=None):
 	data = {
 		'transaction_id': transaction_id,
 		'request_type': request_type,
@@ -57,7 +59,7 @@ def make_request(transaction_id, request_type, user=None, stock_id=None, amount=
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	# Connect the socket to the port where the server is listening
-	server_address = (web_server_address, web_server_port)
+	server_address = (web_server_address, web_server_port+pid)
 	print >>sys.stderr, 'connecting to %s port %s' % server_address
 	sock.connect(server_address)
 
@@ -114,7 +116,7 @@ def processWorkloadFile(sourceDir, targetDir, workloadFile):
 
 
 
-def sendWorkload(user):
+def sendWorkload(user, pid):
 	bad_chars = '[]'
 
 	f = open('./seperatedWorkload/' + user + '.txt', 'r')
@@ -132,109 +134,110 @@ def sendWorkload(user):
 		if request_type == ADD:
 			user = request[1]
 			amount = request[2]
-			make_request(transaction_id, request_type, user, amount=amount)
+			make_request(pid, transaction_id, request_type, user, amount=amount)
 
 		elif request_type == QUOTE:
 			user = request[1]
 			stock_id = request[2]
-			make_request(transaction_id, request_type, user, stock_id)
+			make_request(pid, transaction_id, request_type, user, stock_id)
 
 		elif request_type == BUY:
 			user = request[1]
 			stock_id = request[2]
 			amount = request[3]
-			make_request(transaction_id, request_type, user, stock_id, amount)
+			make_request(pid, transaction_id, request_type, user, stock_id, amount)
 
 		elif request_type == COMMIT_BUY:
 			user = request[1]
-			make_request(transaction_id, request_type, user)
+			make_request(pid, transaction_id, request_type, user)
 			
 		elif request_type == CANCEL_BUY:
 			user = request[1]
-			make_request(transaction_id, request_type, user)
+			make_request(pid, transaction_id, request_type, user)
 			
 		elif request_type == SELL:
 			user = request[1]
 			stock_id = request[2]
 			amount = request[3]
-			make_request(transaction_id, request_type, user, stock_id, amount)
+			make_request(pid, transaction_id, request_type, user, stock_id, amount)
 
 		elif request_type == COMMIT_SELL:
 			user = request[1]
-			make_request(transaction_id, request_type, user)
+			make_request(pid, transaction_id, request_type, user)
 
 		elif request_type == CANCEL_SELL:
 			user = request[1]
-			make_request(transaction_id, request_type, user)
+			make_request(pid, transaction_id, request_type, user)
 
 		elif request_type == SET_BUY_AMOUNT:
 			user = request[1]
 			stock_id = request[2]
 			amount = request[3]
-			make_request(transaction_id, request_type, user, stock_id, amount)
+			make_request(pid, transaction_id, request_type, user, stock_id, amount)
 			
 		elif request_type == CANCEL_SET_BUY:
 			user = request[1]
 			stock_id = request[2]
-			make_request(transaction_id, request_type, user, stock_id)
+			make_request(pid, transaction_id, request_type, user, stock_id)
 			
 		elif request_type == SET_BUY_TRIGGER:
 			user = request[1]
 			stock_id = request[2]
 			amount = request[3]
-			make_request(transaction_id, request_type, user, stock_id, amount)
+			make_request(pid, transaction_id, request_type, user, stock_id, amount)
 			
 		elif request_type == SET_SELL_AMOUNT:
 			user = request[1]
 			stock_id = request[2]
 			amount = request[3]
-			make_request(transaction_id, request_type, user, stock_id, amount)
+			make_request(pid, transaction_id, request_type, user, stock_id, amount)
 			
 		elif request_type == SET_SELL_TRIGGER:
 			user = request[1]
 			stock_id = request[2]
 			amount = request[3]
-			make_request(transaction_id, request_type, user, stock_id, amount)
+			make_request(pid, transaction_id, request_type, user, stock_id, amount)
 			
 		elif request_type == CANCEL_SET_SELL:
 			user = request[1]
 			stock_id = request[2]
-			make_request(transaction_id, request_type, user, stock_id)
+			make_request(pid, transaction_id, request_type, user, stock_id)
 			
 		elif request_type == DUMPLOG:
 			if len(request) == 2:
 				#filename
 				filename = request[1]
-				make_request(transaction_id, request_type, filename=filename)
+				make_request(pid, transaction_id, request_type, filename=filename)
 			elif len(request) == 3:
 				#userid, filename
 				user = request[1]
 				filename = request[2]
-				make_request(transaction_id, request_type, user, filename=filename)
+				make_request(pid, transaction_id, request_type, user, filename=filename)
 
 		elif request_type == DISPLAY_SUMMARY:
 			user = request[1]
-			make_request(transaction_id, request_type, user)
+			make_request(pid, transaction_id, request_type, user)
 
 		else:
 			# INVALID REQUEST
 			print "invalid request: " + request[0]
 
 
-def worker():
+def worker(id):
 	while True:
 	    user = q.get()
-	    sendWorkload(user)
+	    process = id
+	    sendWorkload(user, process)
 	    q.task_done()
 
 def main():
-	userList = processWorkloadFile('/','./seperatedWorkload/','1userWorkLoad.txt')
+	userList = processWorkloadFile('/','./seperatedWorkload/','10User_testWorkLoad.txt')
 
 
 	for i in range(NUM_WORKER_THREADS):
-	     t = Thread(target=worker)
-	     t.daemon = True
-	     t.start()
+		t = Thread(target=worker, args=(i,))
+		t.daemon = True
+		t.start()
 
 	for item in userList:
 	    q.put(item)
