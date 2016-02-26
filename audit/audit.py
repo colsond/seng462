33ccbe83437ef993243f4e416fe1869c71f37a04
Shaun Aiken <swbaiken@uvic.ca>
2016-02-26 12:25:29 -0800

    Revert "trying threaded audit logging"
    This reverts commit c54083eb7f29d2056e346887db49b5a3d2a9d514.
    
    # Conflicts:
    #	quote/quote_cache.py

--- audit/audit.py
+++ audit/audit.py
@@ -171,104 +171,99 @@ def parseErrorEvent(entryDict):
 
 ##Debug Type
 def parseDebug(entryDict):
-	#inputs
-	timeStamp = entryDict['timestamp']
-	server = entryDict['server']
-	transactionNum = entryDict['transactionNum']
-	command = entryDict['command']
-	userName = entryDict.get('username', "")
-	stockSymbol = entryDict.get('stockSymbol', "")
-	fileName = entryDict.get('filename', "")
-	funds = entryDict.get('funds', "")
-	debugMessage = entryDict.get('debugMessage', "")
+    #inputs
+    timeStamp = entryDict['timestamp']
+    server = entryDict['server']
+    transactionNum = entryDict['transactionNum']
+    command = entryDict['command']
+    userName = entryDict.get('username', "")
+    stockSymbol = entryDict.get('stockSymbol', "")
+    fileName = entryDict.get('filename', "")
+    funds = entryDict.get('funds', "")
+    debugMessage = entryDict.get('debugMessage', "")
 
-	DebugType = ''
-	DebugType += '<debugEvent>\n'
-	DebugType += '    <timestamp>' + str(timeStamp) + '</timestamp>\n'
-	DebugType += '    <server>' + server + '</server>\n'
-	DebugType += '    <transactionNum>' + transactionNum + '</transactionNum>\n'
-	DebugType += '    <command>' + command + '</command>\n'
-	if (userName!=""):
-		DebugType += '    <username>' + userName + '</username>\n'
-	if (stockSymbol!=""):
-		DebugType += '    <stockSymbol>' + stockSymbol + '</stockSymbol>\n'
-	if (filename!=""):
-		DebugType += '    <filename>' + filename + '</filename>\n'
-	if (funds!=""):
-		DebugType += '    <funds>' + str(funds) + '</funds>\n'
-	if (debugMessage!=""):
-		DebugType += '    <debugMessage>' + debugMessage + '</debugMessage>\n'
-	DebugType += '</debugEvent>\n'
-
-	return DebugType
-
+    DebugType = ''
+    DebugType += '<debugEvent>\n'
+    DebugType += '    <timestamp>' + str(timeStamp) + '</timestamp>\n'
+    DebugType += '    <server>' + server + '</server>\n'
+    DebugType += '    <transactionNum>' + transactionNum + '</transactionNum>\n'
+    DebugType += '    <command>' + command + '</command>\n'
+    if (userName!=""):
+        DebugType += '    <username>' + userName + '</username>\n'
+    if (stockSymbol!=""):
+        DebugType += '    <stockSymbol>' + stockSymbol + '</stockSymbol>\n'
+    if (filename!=""):
+        DebugType += '    <filename>' + filename + '</filename>\n'
+    if (funds!=""):
+        DebugType += '    <funds>' + str(funds) + '</funds>\n'
+    if (debugMessage!=""):
+        DebugType += '    <debugMessage>' + debugMessage + '</debugMessage>\n'
+    DebugType += '</debugEvent>\n'
+    
+    return DebugType
 # This function handles the data package recieved thru the socket and dumps it into the audit log
 def handleEntry(strdict):
-	xmlPacket = ''
-	if __debug__:
-		print strdict
-	#unpack string into dictionary
-	entryDict = ast.literal_eval(strdict)
+    xmlPacket = ''
+    #print strdict
+    #unpack string into dictionary
+    entryDict = ast.literal_eval(strdict)
+    
+    #based on source parse the dict into an xml string and add it to the log
+    logType = entryDict['logType']
+   
+    #based on log type call the appropriate function to generate xml packet
+    if(logType == "UserCommandType"):
+        xmlPacket = parseUserCommand(entryDict)
+    elif(logType == "QuoteServerType"):
+        xmlPacket = pareseQuoteServer(entryDict)
+    elif(logType == "AccountTransactionType"):
+        xmlPacket = parseAccountTransaction(entryDict)
+    elif(logType == "SystemEventType"):
+        xmlPacket = parseSystemEvent(entryDict)
+    elif(logType == "ErrorEventType"):
+        xmlPacket = parseErrorEvent(entryDict)
+    elif(logType == "DebugType"):
+        xmlPacket = parseDebug(entryDict)
+    else:
+        print "unknown log type"
+        return "UNKNOWN_LOG_TYPE"
+    #unknown log type ?throw an error?
 
-	#based on source parse the dict into an xml string and add it to the log
-	logType = entryDict['logType']
+    #open log file to append to, may need to put this in a try block
+    f = open('logfile.xml', 'a')     
+    
+    #for debugging
+    #print xmlPacket
+    
+    f.write(xmlPacket)
+    f.close() 
+    return "OK"
 
-	#based on log type call the appropriate function to generate xml packet
-	if(logType == "UserCommandType"):
-		xmlPacket = parseUserCommand(entryDict)
-	elif(logType == "QuoteServerType"):
-		xmlPacket = pareseQuoteServer(entryDict)
-	elif(logType == "AccountTransactionType"):
-		xmlPacket = parseAccountTransaction(entryDict)
-	elif(logType == "SystemEventType"):
-		xmlPacket = parseSystemEvent(entryDict)
-	elif(logType == "ErrorEventType"):
-		xmlPacket = parseErrorEvent(entryDict)
-	elif(logType == "DebugType"):
-		xmlPacket = parseDebug(entryDict)
-	else:
-		if __debug__:
-			print "unknown log type"
-		return "UNKNOWN_LOG_TYPE"
-	#unknown log type ?throw an error?
 
-	#open log file to append to, may need to put this in a try block
-	f = open('logfile.xml', 'a')     
-
-	if __debug__:
-		print xmlPacket
-
-	f.write(xmlPacket)
-	f.close() 
-	return "OK"
 
 #Function for handling connections. This will be used to create threads
 def clientthread(conn):
-	#Sending message to connected client
-	#infinite loop so that function do not terminate and thread do not end.
-	while True:
-	#Receiving from client
-	#this is where all the logic for logging will go.
-	  data = conn.recv(4096)
-	#handle request here
-	  if not data: 
-			break
-	#this function call handles the data package and returns ok or gives an unknown log error
-	reply = handleEntry(data)
-	
-	if __debug__:
-		conn.sendall(reply)
-	else:
-		conn.sendall(' ')
-
-	#came out of loop
-	print "Ending transmission"
-	conn.close()
-	sys.exit(0) 
-
+    #Sending message to connected client
+    #infinite loop so that function do not terminate and thread do not end.
+    while True:
+         
+        #Receiving from client
+    #this is where all the logic for logging will go.
+        data = conn.recv(4096)
+    #handle request here
+        if not data: 
+            break
+    #this function call handles the data package and returns ok or gives an unknown log error
+    	reply = handleEntry(data) 
+        conn.sendall(reply)
+     
+    #came out of loop
+    print "Ending transmission"
+    conn.close()
+    sys.exit(0) 
 #now keep talking with the client
 f = open('logfile.xml', 'a')
-f.write('<?xml version="1.0"?><log>\n')
+f.write('<?xml version="1.0"?><log>')
 f.close()
 while 1:
     try:
