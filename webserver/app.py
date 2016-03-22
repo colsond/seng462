23674e39688522c077ccd68bb84ba37c10629139
Curtis Smith <curtis.smith8@gmail.com>
2016-03-22 16:40:08 -0700

    basic webserver set up, need to integrate with workload generator and add load balancing, and ssl?

---
+++ webserver/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, jsonify
+import random
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/trader')
+def trader():
+    print 'trader times?!'
+    return render_template('trader.html')
+
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+
+    request_json = request.get_json()
+    print request_json
+    username = request_json.get("username", None)
+    if username:
+        print username
+    else:
+        print "No usename found"
+
+    request_type = request_json.get("request_type", None)
+    if request_type:
+        print request_type
+    else:
+        print "No request_type found"
+
+    tx_server = random.randint(1, 10)
+    return jsonify({"tx_server": tx_server})
+
+
+if __name__ == "__main__":
+    app.run()
--- webserver/webserver.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def transaction_request():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
