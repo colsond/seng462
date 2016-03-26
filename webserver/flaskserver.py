3c167e7bc1ff88e299d257f59b080d56d9715ab1
Curtis Smith <curtis.smith8@gmail.com>
2016-03-25 18:18:17 -0700

    rename files

--- webserver/flaskserver.py
+++ webserver/flaskserver.py
@@ -5,12 +5,14 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    #return render_template('index.html')
+    return 'whats up'
 
 @app.route('/trader')
 def trader():
     print 'trader times?!'
-    return render_template('trader.html')
+    #return render_template('trader.html')
+    return 'trader?'
 
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
@@ -34,4 +36,4 @@ def authenticate():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='44442')
+    app.run()
--- webserver/wsgi.py
+++
@@ -1,5 +0,0 @@
-from gevent.wsgi import WSGIServer
-from app import app
-
-http_server = WSGIServer(('', 44442), app)
-http_server.serve_forever()
